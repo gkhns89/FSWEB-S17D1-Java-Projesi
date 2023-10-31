db34ef44a1121395d3e1fe3318154451e77f4ffd
gkhns89 <gokhansisman@outlook.com>
2023-10-31 23:52:05 +0300

    Initial commit!

---
+++ src/main/java/com/s171/spring1start/Spring1StartApplication.java
@@ -0,0 +1,13 @@
+package com.s171.spring1start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1StartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s171/spring1start/Spring1StartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s171.spring1start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1StartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
